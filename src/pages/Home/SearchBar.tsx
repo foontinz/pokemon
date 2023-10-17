11a6ac632ceea9168b9978b2e6b4417305a2f06b
Dmytro Stoianov <dima22022005@gmail.com>
2023-10-17 12:30:36 +0200

    Create a searchbar component.

--- src/pages/Home/SearchBar.tsx
+++ src/pages/Home/SearchBar.tsx
@@ -1,11 +1,17 @@
+import Container from "@/components/ui/Container"
+import Icon from "@/components/ui/Icon"
+
 type SearchBarProps = {}
 
 function SearchBar({}: SearchBarProps) {
     return (
-        <div>
-
-        </div>
-    );
+        <Container className="flex justify-center">
+            <div className="w-full md:w-[600px] h-[80px] rounded-[25px] shadow-md bg-gray flex items-center justify-between gap-4 lg:px-4 px-6">
+                <Icon name="Search" size="medium" />
+                <input className="bg-white w-full lg:w-[520px] h-[50px] text-center rounded-[25px] px-4 font-glory text-lg"></input>
+            </div>
+        </Container>
+    )
 }
 
-export default SearchBar;
+export default SearchBar
