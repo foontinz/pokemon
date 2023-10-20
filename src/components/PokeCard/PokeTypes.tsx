ddd995401b866f2dd38a9b70b4910d5dd9dbd95d
Dmytro Stoianov <dima22022005@gmail.com>
2023-10-20 20:31:43 +0200

    Add poke types design and add all pokemon types to tailwind config colors.

--- src/components/PokeCard/PokeTypes.tsx
+++ src/components/PokeCard/PokeTypes.tsx
@@ -3,13 +3,51 @@ type PokeTypesProps = {
 }
 
 function PokeTypes({ types }: PokeTypesProps) {
-    return (
-        <>
-            {types.map((type, idx) => (
-                <p className="font-glory text-brown" key={idx}>{type}</p>
-            ))}
-        </>
-    )
+    switch (types.length) {
+        case 1:
+            return (
+                <div
+                    className={`w-[10rem] h-[2.5rem] rounded-[25px] shadow-md flex justify-center items-center bg-pokemon-${types[0]}`}
+                >
+                    <p className="text-white">{types[0]}</p>
+                </div>
+            )
+        case 2:
+            return (
+                <div className="w-[10rem] h-[2.5rem] bg-transparent flex justify-center items-center gap-1">
+                    <div
+                        className={`flex w-[50%] h-full justify-center shadow-md rounded-tl-[25px] rounded-bl-[25px] items-center bg-pokemon-${types[0]}`}
+                    >
+                        <p className="text-white">{types[0]}</p>
+                    </div>
+                    <div
+                        className={`flex w-[50%] h-full justify-center shadow-md rounded-tr-[25px] rounded-br-[25px] items-center bg-pokemon-${types[1]}`}
+                    >
+                        <p className="text-white">{types[1]}</p>
+                    </div>
+                </div>
+            )
+        case 3:
+            return (
+                <div className="w-[10rem] h-[2.5rem] bg-transparent flex justify-center items-center gap-1">
+                    <div
+                        className={`flex w-[33%] h-full justify-center shadow-md rounded-tl-[25px] rounded-bl-[25px] items-center bg-pokemon-${types[0]}`}
+                    >
+                        <p className="text-white">{types[0]}</p>
+                    </div>
+                    <div
+                        className={`flex w-[33%] h-full justify-center shadow-md items-center bg-pokemon-${types[1]}`}
+                    >
+                        <p className="text-white">{types[1]}</p>
+                    </div>
+                    <div
+                        className={`flex w-[33%] h-full justify-center shadow-md rounded-tr-[25px] rounded-br-[25px] items-center bg-pokemon-${types[2]}`}
+                    >
+                        <p className="text-white">{types[2]}</p>
+                    </div>
+                </div>
+            )
+    }
 }
 
 export default PokeTypes
