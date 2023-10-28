77ee899ab7b3767f395b8d9eec3ef40b94495d60
foontinz <foontinz@protonmail.com>
2023-10-28 14:40:28 +0200

    Adds GET_POKEMONS by type,gen and regexp gql in services/pokedex/operations

--- src/services/pokedex/operations/pokemons.ts
+++ src/services/pokedex/operations/pokemons.ts
@@ -1,14 +1,55 @@
 import gql from 'graphql-tag';
 
 export const GET_POKEMONS = gql`
-query GET_POKEMONS($limit: Int!, $offset: Int!) {
-  pokemons: pokemon_v2_pokemon(limit: $limit, offset: $offset) {
+query GET_POKEMONS($limit: Int!, $offset: Int!, $order_by: [pokemon_v2_pokemonspecies_order_by!]) {
+  pokemons: pokemon_v2_pokemonspecies(order_by: $order_by, limit: $limit, offset: $offset) {
     id
     name
-    order
-    pokemon_v2_pokemontypes {
-      pokemon_v2_type {
-        name
+    pokemon_details: pokemon_v2_pokemons(distinct_on: pokemon_species_id) {
+      types: pokemon_v2_pokemontypes {
+        type_id
+      }
+    }
+  }
+}
+`
+
+export const GET_POKEMONS_BY_GEN = gql`
+query GET_POKEMONS($limit: Int!, $offset: Int!, $order_by: [pokemon_v2_pokemonspecies_order_by!], $generation_id: Int!) {
+  pokemons: pokemon_v2_pokemonspecies(order_by: $order_by, limit: $limit, offset: $offset, where: {generation_id: {_eq: $generation_id}}) {
+    id
+    name
+    pokemon_details: pokemon_v2_pokemons (distinct_on: pokemon_species_id) {
+      types: pokemon_v2_pokemontypes {
+        type_id
+      }
+    }
+  }
+}
+`
+
+export const GET_POKEMONS_BY_TYPE = gql`
+query GET_POKEMONS($limit: Int!, $offset: Int!, $order_by: [pokemon_v2_pokemonspecies_order_by!], $type_id: Int!) {
+  pokemons: pokemon_v2_pokemonspecies(order_by: $order_by, limit: $limit, offset: $offset, where: {pokemon_v2_pokemons: {pokemon_v2_pokemontypes: {type_id: {_eq: $type_id}}}}) {
+    id
+    name
+    pokemon_details: pokemon_v2_pokemons (distinct_on: pokemon_species_id) {
+      types: pokemon_v2_pokemontypes {
+        type_id
+      }
+    }
+  }
+}
+`
+
+export const GET_POKEMONS_REGEX = gql`
+query GET_POKEMONS($limit: Int!, $offset: Int!, $order_by: [pokemon_v2_pokemonspecies_order_by!], $name_regex: String! = "") {
+  pokemons: pokemon_v2_pokemonspecies(order_by: $order_by, limit: $limit, offset: $offset, where: {name: {_iregex: $name_regex}}) {
+    id
+    name
+    pokemon_details: pokemon_v2_pokemons(distinct_on: pokemon_species_id) {
+      types: pokemon_v2_pokemontypes {
+        type_id
       }
     }
   }
