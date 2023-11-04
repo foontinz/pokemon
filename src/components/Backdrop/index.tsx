468daf46cd4b9b56cf5ec47ec3158b36326679cc
Dmytro Stoianov <dima22022005@gmail.com>
2023-11-04 01:46:39 +0100

    Fix bug with opacity.
    Modal background opacity was affecting the poke modal. Therefore change it background to be rgba value with opacity 60%.

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -10,9 +10,9 @@ function Backdrop({ children, onClick }: BackdropProps) {
     return (
         <motion.div
             onClick={onClick}
-            className="fixed top-0 left-0 w-full h-full z-[100] cursor-default bg-black flex justify-center items-center"
+            className="fixed top-0 left-0 w-full h-full z-[100] cursor-default bg-black/60 flex justify-center items-center"
             initial={{ opacity: 0 }}
-            animate={{ opacity: 0.75 }}
+            animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
             {children}
